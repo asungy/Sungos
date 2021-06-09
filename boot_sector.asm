a5a626c307cbd49b4c97d9b9f7c3204c98391133
Alek Sung <aleksungis@gmail.com>
2021-06-09 17:39:41 -0400

    Added welcome message

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,15 +1,42 @@
 ; Signify the video interrupt (int 0x10) to write contents of `al` in tty mode
 mov ah, 0x0e
 
-; Printing 'hello' to terminal
-mov al, 'H'
+; Print 'Welcome to SungOS' to terminal
+mov al, 'W'
 int 0x10
 mov al, 'e'
 int 0x10
 mov al, 'l'
 int 0x10
+mov al, 'c'
 int 0x10
 mov al, 'o'
+int 0x10
+mov al, 'm'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 't'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'S'
+int 0x10
+mov al, 'u'
+int 0x10
+mov al, 'n'
+int 0x10
+mov al, 'g'
+int 0x10
+mov al, 'O'
+int 0x10
+mov al, 'S'
+int 0x10
+mov al, 0x21 ; 0x21 == '!'
 int 0x10
 
 ; Jump to current address
