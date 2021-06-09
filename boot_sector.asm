0e742f79c451c41442f6c768f04eb2b1141c4017
Alek Sung <aleksungis@gmail.com>
2021-06-09 12:00:55 -0400

    boot loader prints hello

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,6 +1,19 @@
-; Infinite loop (e9 fd ff)
-loop:
-    jmp loop
+; Signify the video interrupt (int 0x10) to write contents of `al` in tty mode
+mov ah, 0x0e
+
+; Printing 'hello' to terminal
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10
+mov al, 'o'
+int 0x10
+
+; Jump to current address
+jmp $
 
 ; Fill with 510 zeros minus the size of the previous code
 times 510-($-$$) db 0
