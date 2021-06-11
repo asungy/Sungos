df34940574f178ae354746ce74c2e795a4cd8bc2
Alek Sung <aleksungis@gmail.com>
2021-06-11 18:50:54 -0400

    Fixed bug with print_string
    Did not push registers beforehand so the return address was not saved.

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,16 +1,21 @@
-; Signify the video interrupt (int 0x10) to write contents of `al` in tty mode
-mov ah, 0x0e
-
 mov di, welcome_string
 call print_string
 
+mov di, hello_world
+call print_string
+
 welcome_string:
-  db 'Welcome to SungOS!',0
+  db 'Welcome to SungOS',0x0
+
+hello_world:
+  db 'Hello world',0x0
 
 print_string:
+  pusha
+  ; Signify the video interrupt (int 0x10) to write contents of `al` in tty mode
+  mov ah, 0x0e
   ; Index counter
   mov cx, 0x0
-  ; Prints 'Welcome to SungOS' to terminal
   L1:
     ; Print character to terminal
     mov bx, 0x7c00
@@ -22,8 +27,8 @@ print_string:
     ; Check if encounter null-terminator
     cmp [bx], BYTE 0x0
     jne L1
-    popa
-    ret
+  popa
+  ret
 
 ; Jump to current address
 jmp $
